bd8ce0b4f02b6728cd6a82dc716af084c2957bc2
abdurrahimkizilkaya <abdurrahimkizilkaya@gmail.com>
2023-04-13 23:47:33 +0300

    RxJava Stream type changed from"Observable" to "Single"

--- app/src/main/java/com/solocatapps/mvvmproductlistapp/service/ProductApi.kt
+++ app/src/main/java/com/solocatapps/mvvmproductlistapp/service/ProductApi.kt
@@ -2,12 +2,11 @@ package com.solocatapps.mvvmproductlistapp.service
 
 import com.solocatapps.mvvmproductlistapp.model.ProductResponse
 import com.solocatapps.mvvmproductlistapp.util.Constants.GET_ALL_PRODUCTS
-import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
 
 interface ProductApi {
 
     @GET(GET_ALL_PRODUCTS)
-    fun getAllProducts() : Observable<ProductResponse>
-
+    fun getAllProducts() : Single<ProductResponse>
 }
